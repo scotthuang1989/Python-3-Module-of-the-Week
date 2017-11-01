d683d07acb1f33f2b5fc8fbb7bf25cce2c9df015
scott huang <scotthuang1989@163.com>
2017-11-01 14:09:50 +0800

    asyncio

---
+++ concurrency/asyncio/asyncio_call_later.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+def callback(n):
+    print('callback {} invoked'.format(n))
+
+
+async def main(loop):
+    print('registering callbacks')
+    loop.call_later(0.2, callback, 1)
+    loop.call_later(0.1, callback, 2)
+    loop.call_soon(callback, 3)
+
+    await asyncio.sleep(0.4)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    print('entering event loop')
+    event_loop.run_until_complete(main(event_loop))
+finally:
+    print('closing event loop')
+    event_loop.close()
